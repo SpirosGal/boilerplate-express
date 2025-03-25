94e7a52c6e9653dd8bb474f64848a966416c594f
Spiros Galeros <spirosgaleros@gmail.com>
2025-03-25 15:47:46 +0000

    modified:   myApp.js

--- myApp.js
+++ myApp.js
@@ -1,36 +1,13 @@
 let express = require('express');
 let app = express();
 
+// Log to the console when the app starts
 console.log("Hello Express");
 
+// Define a route for the root path '/'
+app.get('/', (req, res) => {
+  res.send('Hello Express');
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Export the app for use in the server
 module.exports = app;
