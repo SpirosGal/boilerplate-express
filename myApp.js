1f34f8d4a7822bbd05680d584a2e184c46ead0f1
Spiros Galeros <spirosgaleros@gmail.com>
2025-03-25 16:02:16 +0000

    modified:   myApp.js

--- myApp.js
+++ myApp.js
@@ -2,11 +2,11 @@ let express = require('express');
 let app = express();
 
 // Log to the console when the app starts
-console.log("Hello Express");
+console.log("Hello World");
 
 // Define a route for the root path '/'
 app.get('/', (req, res) => {
-  res.send('Hello Express');
+  res.sendFile('/workspace/boilerplate-express/views/index.html');
 });
 
 // Export the app for use in the server
