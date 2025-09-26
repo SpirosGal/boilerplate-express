e02921aebb9ef61fd9447874a95bb866bdb06935
Spiros Galeros <146263709+SpirosGal@users.noreply.github.com>
2025-09-27 00:33:43 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,16 +1,15 @@
 let express = require('express');
-let path = require('path');  
+let path = require('path');
 let app = express();
 
 console.log("Hello World");
 
-// Serve static files from /public
+// Serve static files from /public ONLY at /public
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-// Root route should serve index.html inside /public
+// Root should send index.html (must be inside /public)
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Export app
 module.exports = app;
